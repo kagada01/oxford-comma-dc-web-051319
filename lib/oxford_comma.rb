7abb67cb772ccb1709ee0ebf28b86ee5b4e64e59
Dan Kagan <dankagan7@gmail.com>
2019-04-14 03:02:37 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,20 +1,14 @@
-require pry
+require "pry"
 
 def oxford_comma(array)
   if (array.length <= 1)
     array.join()
   elsif (array.length == 2)
     array.join(" and ")
-  elsif (array.length == 3)
-    joined_array = array.join(", ")
-    split_array = joined_array.split(" ")
-    split_array.insert((array.length - 1), "and")
-    split_array.join(" ")
   else
-    joined_array = array.join(", ")
-    split_array = joined_array.split(" ")
-    binding.pry
-    split_array.insert((array.length - 1), "and")
-    split_array.join(" ")
+    new_array = "and #{array[-1]}"
+    array.pop
+    array.push(new_array)
+    return array.join(", ")
   end
 end
