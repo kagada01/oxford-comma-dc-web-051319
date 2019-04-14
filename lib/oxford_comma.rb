27abf8a0052ae88fd0d8e2055d07d939a4bc1cf1
Dan Kagan <dankagan7@gmail.com>
2019-04-14 03:01:49 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,11 +5,11 @@ def oxford_comma(array)
     array.join()
   elsif (array.length == 2)
     array.join(" and ")
-  elsif (array.length == 3)
-    joined_array = array.join(", ")
-    split_array = joined_array.split(" ")
-    split_array.insert((array.length - 1), "and")
-    split_array.join(" ")
+  #elsif (array.length == 3)
+   # joined_array = array.join(", ")
+    #split_array = joined_array.split(" ")
+    #split_array.insert((array.length - 1), "and")
+    #split_array.join(" ")
   else
     new_array = "and #{array[-1]}"
     array.pop
